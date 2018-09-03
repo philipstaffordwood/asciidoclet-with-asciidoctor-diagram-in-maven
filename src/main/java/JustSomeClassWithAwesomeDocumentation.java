2a49e95b8690c0d68bd54185313be96e30378eff
Philip Wood <philip.wood@gmail.com>
2018-09-03 15:15:20 +0200

    Initial check-in

---
+++ src/main/java/JustSomeClassWithAwesomeDocumentation.java
@@ -0,0 +1,25 @@
+/**
+ * = So this heading
+ * is the introduction to a class that has
+ * **rocking** documentation
+ *
+ * See the obligatory XKCD
+ * https://xkcd.com/1808/
+ * [plantuml, dialog, png]
+ * ....
+ * Title Hacking
+ * actor She
+ * actor He
+ * She -> He : You know how sometimes\npeople put a space in their\nemaill address to make it\nharder to harvest?
+ * She <-- He : Yeah?
+ * She -> He : They have a tool that \ncan //delete the space//!
+ * He -> She : Oh my God
+ * legend
+ *    Less-Dramatic revelations
+ *    from the CIA hacking dump
+ * endlegend
+ * ....
+ *
+ */
+public class JustSomeClassWithAwesomeDocumentation {
+}
